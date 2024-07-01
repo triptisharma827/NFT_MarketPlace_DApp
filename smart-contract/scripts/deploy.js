1738006c7c02522c2278cae0bb0db54383cfebcf
triptisharma827 <triptisharma827@gmail.com>
2024-07-01 16:07:12 +0530

    Initial Commit

---
+++ smart-contract/scripts/deploy.js
@@ -0,0 +1,13 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const Contract = await hre.ethers.getContractFactory("MyERC721");
+
+    const contract = await Contract.deploy();
+    await contract.waitForDeployment();
+
+    console.log("Contract deployed to address:",await contract.getAddress());
+}
+
+main();
